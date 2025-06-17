26c07c3c9414bfaf70657b092a658d8a35dc3b53
JS-codev <lone7walker@gmail.com>
2025-06-17 23:27:24 +0800

    laptop_push

---
+++ Projects/OpenGL/include/config.h
@@ -0,0 +1,23 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <vector>
+
+#include <glad/glad.h>
+#include <GLFW\glfw3.h>
+
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
+
+#include "stb_image.h"
+#include "../header/Window.h"
+#include "../header/Mesh.h"
+#include "../header/Shader.h"
+#include "../header/Camera.h"
+
+#endif // CONFIG_H
