99ca8815236a7a3b11295e49da240e78b4052ebd
zen7 <lone7walker@gmail.com>
2025-07-23 16:00:51 +0800

    sync

--- Online Investment Calculator/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-import logo from '../assets/investment-logo.png';
-export default function Header() {
-    return <header id='header'>
-        <img src={logo} alt=' Investment logo ' />
-        <h1>Online Investment Calculator</h1>
-    </header>
-}
